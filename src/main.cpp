14bd69a074e275c788f74bdb342aaa9b68383783
Евгений <rabkov_evgeniy@mail.ru>
2021-04-17 22:22:09 +0700

    Add comment with code-style rules

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
+// Правила наименований
+
+// lower_case_with_underscores: переменные, функции
+
+// UpperCamelCase: структуры, объединения, перечисления
+
+// UPPER_CASE_WITH_UNDERSCORES: макросы
+
 using namespace std;
 
 int main(int argc, char* argv)
