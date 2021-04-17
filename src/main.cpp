ce70ac04195adc3423b8e61d90a9b2ee5ae8e12e
Евгений <rabkov_evgeniy@mail.ru>
2021-04-17 23:07:36 +0700

    Add comment with recomendations for writing OOP code

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 
-// Правила наименований
+// Правила наименований:
 
 // lower_case_with_underscores: переменные, функции
 
 // UpperCamelCase: структуры, объединения, перечисления
 
 // UPPER_CASE_WITH_UNDERSCORES: макросы
+
+// Общие рекомендации:
+
+// Не создавать композированных классов
+
+// Использовать protected вместо public (Необходимо для тестирования)
 
 using namespace std;
 
