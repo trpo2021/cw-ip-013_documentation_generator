62503c468bce83066bfa5783d446b69f088eda86
molkodmitriy <molko_dmitriy@mail.ru>
2021-04-20 18:23:55 +0700

    Add documentation_function

--- src/AutoDocLib.cpp
+++ src/AutoDocLib.cpp
@@ -7,23 +7,22 @@ void auto_doc(string path)
     file.open(path);
     try {
         string doc;
-        char symbol;
-        while (!file.eof()) {
-            file >> symbol;
-            doc += symbol;
-        }
+        getline(file, doc, '\0');
         file.close();
         p2i position;
         long unsigned int number_comment = 0;
-        do {
+        while (1) {
             position = find_comment(doc, number_comment);
-            number_comment = position.second + 1;
+            if (position == p2i(-1, 2))
+                break;
+            number_comment = position.second;
             if (doc.find("Class", number_comment) == number_comment
                 || doc.find("Struct", number_comment) == number_comment) {
+                documentation_classes(doc, position);
             } else {
+                documentation_functions(doc, position);
             }
-
-        } while (position != p2i(-1, -1));
+        }
 
     } catch (const std::exception& e) {
     }
@@ -31,8 +30,32 @@ void auto_doc(string path)
 
 p2i find_comment(string& doc, int number_comment)
 {
-    int start_comment = doc.find("/*", number_comment);
-    int end_comment = doc.find("*/", number_comment);
+    int start_comment = doc.find("/*!", number_comment);
+    int end_comment = doc.find("!*/", number_comment) + 3;
+    end_comment = doc.find_first_not_of(' ', end_comment);
 
     return p2i(start_comment, end_comment);
+}
+void documentation_functions(string& doc, p2i position)
+{
+    TemplateFuncDoc func_doc;
+    string name;
+    string short_desctiption;
+    string description;
+    int end_name_pos = doc.find(';', position.second);
+    name.append(doc, position.second, end_name_pos - position.second);
+
+    int short_descr_pos = doc.find("#~", position.first) + 2;
+    while (doc[short_descr_pos != '\n']) {
+        short_desctiption.push_back(doc[short_descr_pos]);
+        short_descr_pos++;
+    }
+    short_descr_pos++;
+    int end_comment = doc.find("!*/", position.first) - 1;
+    description.append(doc, short_descr_pos, end_comment - short_descr_pos);
+
+    func_doc.set_func_info(name, short_desctiption, description);
+}
+void documentation_classes(string& doc, p2i position)
+{
 }
