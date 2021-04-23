55c20e0ea548b24237243390133cb6c76151ec77
Simon <kemer0v0@mail.ru>
2021-04-23 20:01:22 +0700

    Add func for make class doc

--- src/TemplateClassDoc.cpp
+++ src/TemplateClassDoc.cpp
@@ -22,22 +22,87 @@ void TemplateClassDoc::add_var_info(string name, string short_description)
 // TODO Добавить шаблон документации в вывод.
 void TemplateClassDoc::make_documentation(string path)
 {
-    ofstream file;
-    file.open(path + "/Class/" + this->info.name + ".html");
+    ofstream fileout;
+    ifstream filein;
+    fileout.open(path + "/Class/" + this->info.name + ".html");
+    filein.open("./src/template/ClassName.html");
 
-    file << this->info.name << " - ";
+    std::string buff;
+    std::getline(filein, buff, '\0');
+    filein.close();
 
-    file << this->info.short_description << endl;
+    int first_field = buff.find("#~");
+    int last_field = buff.find("#~", first_field + 1);
 
-    file << this->info.description << endl;
+    int first_method = buff.find("#*");
+    int last_method = buff.find("#*", first_method + 2);
+
+    for (int i = 0; i < first_field; i++) {
+        if (buff[i] == '#') {
+            switch (buff[i + 1]) {
+            case '1':
+                fileout << this->info.name;
+                break;
+            case '2':
+                fileout << this->info.short_description;
+                break;
+            case '3':
+                fileout << this->info.description;
+                break;
+            default:
+                // throw 1;
+                break;
+            }
+            i++;
+            continue;
+        }
+        fileout << buff[i];
+    }
 
     for (auto it = this->methods_info.begin(); it != this->methods_info.end();
          ++it) {
-        file << it->name << " - " << it->short_description << endl;
+        for (int i = first_field + 2; i < last_field; i++) {
+            if (buff[i] == '#') {
+                switch (buff[i + 1]) {
+                case '4':
+                    fileout << it->name;
+                    break;
+                case '5':
+                    fileout << it->short_description;
+                    break;
+                }
+                i++;
+                continue;
+            }
+            fileout << buff[i];
+        }
+    }
+
+    for (int i = last_field + 2; i < first_method; i++) {
+        fileout << buff[i];
     }
 
     for (auto it = this->vars_info.begin(); it != this->vars_info.end(); ++it) {
-        file << it->name << " - " << it->short_description << endl;
+        for (int i = first_method; i < last_method; i++) {
+            if (buff[i] == '#') {
+                switch (buff[i + 1]) {
+                case '6':
+                    fileout << it->name;
+                    break;
+                case '7':
+                    fileout << it->short_description;
+                    break;
+                }
+                i++;
+                continue;
+            }
+            fileout << buff[i];
+        }
     }
-    file.close();
+
+    for (int i = last_method + 2; i < buff.size(); i++) {
+        fileout << buff[i];
+    }
+
+    fileout.close();
 }
